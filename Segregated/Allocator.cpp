47b15a1c600dd4c79c7cd057ecf6def96c05184e
Mtrqq <40215837+Mtrqq@users.noreply.github.com>
2019-07-15 14:32:02 +0300

    1

--- Segregated/Allocator.cpp
+++ Segregated/Allocator.cpp
@@ -112,7 +112,7 @@ void* CustomAllocator::Allocator::GetSuitablePosition(void* ip_memory_class_posi
 			*bit_field &= ~checked_bit;
 			std::cout << std::endl;
 			auto offset = static_cast<long long>(i_class_block_size) * position_from_start;
-			auto system_offset = (mg_memory_per_class / i_class_block_size) - GetRealAmountOfBlocks(i_class_block_size)
+			auto system_offset = (mg_memory_per_class / i_class_block_size) - GetRealAmountOfBlocks(i_class_block_size);
 			return GetShiftedPointer(ip_memory_class_position, offset + system_offset * i_class_block_size);
 		}
 		checked_bit >>= 1;
